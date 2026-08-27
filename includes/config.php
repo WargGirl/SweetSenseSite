ef768fe6b1e67edbd91cef07a2ab8a63e8166334
WargGirl <gashenkoalisa@gmail.com>
2026-08-27 06:38:16 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,5 +7,11 @@ define('CLASSES_PATH', ROOT_PATH . '/classes');
 define('LANG_PATH', ROOT_PATH . '/lang');
 
 define('SITE_NAME', 'SweetSense');
-define('BASE_URL', '/SweetSense/public');
 define('DEFAULT_LANG', 'uk');
+define('BASE_URL', getenv('RAILWAY_ENVIRONMENT') ? '' : '/SweetSense/public');
+
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'counter');
+define('DB_PORT', getenv('DB_PORT') ?: 3306);
